758c0ce76ddd0601b384cbb526c26b6b444e66b6
shraddhaw9604-hub <shraddha.w9604@gmail.com>
2026-02-05 21:01:02 +0530

    Update guess_number.py

--- guess_number.py
+++ guess_number.py
@@ -1,10 +1,8 @@
 import random
 print("Welcome to Number Guessing Game")
 number = random.randint(1, 10)
-
 while True:
     guess = int(input("Guess a number between 1 and 10: "))
-
     if guess == number:
         print("Correct! You guessed the number 🎉")
         break
